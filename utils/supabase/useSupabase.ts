d89055380393838b17b0fc08687022410dbfab4b
cichowlasp <cichowlas.p@gmail.com>
2025-01-17 20:11:07 +0100

    fixed logout issue

--- utils/supabase/useSupabase.ts
+++ utils/supabase/useSupabase.ts
@@ -1,9 +1,11 @@
 'use client';
 
 import { createClient } from './client';
+import { redirect } from 'next/navigation';
 
 export const supabase = createClient();
 
 export const signOut = async () => {
 	await supabase.auth.signOut();
+	redirect('/');
 };
