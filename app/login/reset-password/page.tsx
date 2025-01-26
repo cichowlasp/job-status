19c109fb37319c44e9515ef0d55f0cf04b11d128
cichowlasp <cichowlas.p@gmail.com>
2025-01-26 15:34:40 +0100

    fixed appos error

--- app/login/reset-password/page.tsx
+++ app/login/reset-password/page.tsx
@@ -84,8 +84,8 @@ export default function ResetPasswordPage() {
 							Reset password
 						</CardTitle>
 						<CardDescription>
-							Enter your email address and we'll send you a link
-							to reset your password
+							Enter your email address and we&apos;ll send you a
+							link to reset your password
 						</CardDescription>
 					</CardHeader>
 					<CardContent>
